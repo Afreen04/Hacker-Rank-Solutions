f36c47511f6d0640a4b50e76caca4b884929e3bd
afreen04 <afreenwins@gmail.com>
2017-01-18 20:19:18 -0600

    This is printing output in reverse order. Trying to fix by storing the output and then printing all at the end.

---
+++ Cracking The Coding Interview/Tries-Contacts.cpp
@@ -0,0 +1,110 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+int findContacts(string name, vector<string> contacts)
+{   int ctr = 0;
+    for(int i =0; i<contacts.size();i++)
+     {
+        if(contacts[i].find(name))
+            ctr++;
+    }
+    return ctr;
+}    
+
+void addContact(string name, vector<string> &contacts)
+{
+        contacts.insert(contacts.end(),name);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<string> contacts;
+    for(int a0 = 0; a0 < n; a0++){
+        string op;
+        string contact;
+        cin >> op >> contact;
+        if(op.compare("add") == 0)
+            addContact(contact, contacts);
+        else
+            cout<<findContacts(contact, contacts)<<"\n";
+    }
+    return 0;
+}
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+int findContacts(string name, vector<string> contacts)
+{   int ctr = 0;
+    for(int i =0; i<contacts.size();i++)
+     {
+        if(contacts[i].find(name))
+            ctr++;
+    }
+    return ctr;
+}    
+
+void addContact(string name, vector<string> &contacts)
+{
+        contacts.insert(contacts.end(),name);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<string> contacts;
+    for(int a0 = 0; a0 < n; a0++){
+        string op;
+        string contact;
+        cin >> op >> contact;
+        if(op.compare("add") == 0)
+            addContact(contact, contacts);
+        else
+            cout<<findContacts(contact, contacts)<<"\n";
+    }
+    return 0;
+}
